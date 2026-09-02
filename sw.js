ddd3ad4084f8d30861514371e0c7f5932bfffc4c
golovalisaia-hub <golovalisaia@gmail.com>
2026-09-03 02:04:24 +0300

    Уточнить прогресс на сегодня: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sever-v12';
-const ASSETS=['./','./index.html','./style.css?v=12','./app.js?v=12','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='sever-v13';
+const ASSETS=['./','./index.html','./style.css?v=13','./app.js?v=13','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>event.waitUntil(
   caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
