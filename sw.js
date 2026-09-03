64e4670332f99c12079d27e336395bb3fbe7716a
golovalisaia-hub <golovalisaia@gmail.com>
2026-09-03 11:40:31 +0300

    Update SEVER v16: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sever-v15';
-const ASSETS=['./','./index.html','./style.css?v=15','./app.js?v=15','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='sever-v16';
+const ASSETS=['./','./index.html','./style.css?v=16','./app.js?v=16','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>event.waitUntil(
   caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
