72036e42c1a141216a248242a7d3922e49278578
golovalisaia-hub <golovalisaia@gmail.com>
2026-09-03 11:01:55 +0300

    Добавить заметки, отдельный таймер и понятные задачи: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sever-v13';
-const ASSETS=['./','./index.html','./style.css?v=13','./app.js?v=13','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='sever-v14';
+const ASSETS=['./','./index.html','./style.css?v=14','./app.js?v=14','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>event.waitUntil(
   caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
